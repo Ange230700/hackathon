3622fedb73ce35a4ab56143156af8013e6f5f873
Aurelien <aurelien.emeriau@hotmail.fr>
2024-01-18 11:32:22 +0100

    pages images

--- frontend/src/pages/Image3.jsx
+++ frontend/src/pages/Image3.jsx
@@ -1,18 +1,3 @@
-// function Image3() {
-//   return (
-//     <div className="Iphone14Plus3">
-//       <img
-//         className="GlobalWomenBeautyStandardsEstherHonig23"
-//         src="./src/assets/images/Original.jpg"
-//         alt="image3"
-//       />
-//       <div className="loader" />
-//     </div>
-//   );
-// }
-
-// export default Image3;
-
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
