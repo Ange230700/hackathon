010c2c79776cf25a9e6cdc8a31fe044b087c6669
CarolineG85 <caroline.gallix85@gmail.com>
2024-01-18 11:07:32 +0100

    move panier component from app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Panier from "./pages/Panier";
 
 function App() {
   return (
     <div className="App">
       <Outlet />
-      <Panier />
     </div>
   );
 }
