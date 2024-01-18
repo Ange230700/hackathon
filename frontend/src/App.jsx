0f35d42516351b48eee9f3f8629ebd8c0c6a0107
CarolineG85 <caroline.gallix85@gmail.com>
2024-01-18 11:02:35 +0100

    finished panier page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Panier from "./pages/Panier";
 
 function App() {
   return (
     <div className="App">
       <Outlet />
+      <Panier />
     </div>
   );
 }
