da0fdca5df73062d1057675f64755b994a1454de
CarolineG85 <caroline.gallix85@gmail.com>
2024-01-18 13:54:40 +0100

    finished routine

--- frontend/src/pages/Routine.jsx
+++ frontend/src/pages/Routine.jsx
@@ -1,31 +1,59 @@
+import { useState } from "react";
 import Logo from "../components/Logo";
+import Basket from "../components/Basket";
 
 function Routine() {
+  const [basketAmount] = useState(28.11);
+
   return (
-    <div className="routine-page">
-      <Logo />
-      <div className="header-routine">
-        <p>Votre routine soin sur-mesure</p>
-      </div>
-      <div className="productDiv">
-        <div className="productInfo">
-          <div className="productImageDiv">
-            <img
-              className="productImg"
-              src="../src/assets/images/fond_de_teint.jpg"
-              alt="fond_de_teint"
-            />
+    <>
+      <div className="routine-page">
+        <Logo />
+        <div className="header-routine">
+          <p>Votre routine soin sur-mesure</p>
+        </div>
+        <div className="products-container">
+          <div className="productDiv">
+            <div className="productInfo">
+              <div className="productImageDiv">
+                <img
+                  className="productImg"
+                  src="../src/assets/images/revitalift.png"
+                  alt="fond_de_teint"
+                />
+              </div>
+              <div className="productDetailsDiv">
+                <p className="productName">Sérum Make International</p>
+                <p className="productPrice">13.12€</p>
+              </div>
+            </div>
+            <div className="addToBasket">
+              <p>AJOUTER AU PANIER</p>
+            </div>
           </div>
-          <div className="productDetailsDiv">
-            <p className="productName">Sérum teinté Accord Parfait</p>
-            <p className="productPrice">13.12€</p>
+          <div className="productDiv">
+            <div className="productInfo">
+              <div className="productImageDiv">
+                <img
+                  className="productImg"
+                  src="../src/assets/images/contouryeux.jpg"
+                  alt="fond_de_teint"
+                />
+              </div>
+              <div className="productDetailsDiv">
+                <p className="productName">Contour pour les yeux List Rule</p>
+                <p className="productPrice">14.99€</p>
+              </div>
+            </div>
+            <div className="addToBasket">
+              <p>AJOUTER AU PANIER</p>
+            </div>
           </div>
         </div>
-        <div className="addToBasket">
-          <p>AJOUTER AU PANIER</p>
-        </div>
       </div>
-    </div>
+
+      <Basket basketAmount={basketAmount} />
+    </>
   );
 }
 
